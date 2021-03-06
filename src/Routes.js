701e18caca92ef2572d5799cc73796c5e4416f0d
Jasper Alblas <just_jasper@hotmail.com>
2021-03-06 22:43:15 +0100

    added location page with leaflet map

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import App from "./App";
 import Profile from "./Profile";
+import Location from "./Location";
 
 const Routes = () => {
   return (
@@ -9,6 +10,7 @@ const Routes = () => {
       <Switch>
         <Route exact path="/" component={App} />
         <Route exact path="/profile" component={Profile} />
+        <Route exact path="/location" component={Location} />
       </Switch>
     </BrowserRouter>
   );
