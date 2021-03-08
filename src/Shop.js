771e344632c6c69539c4d1dd36fcb2ae8e73a74b
Jasper Alblas <just_jasper@hotmail.com>
2021-03-08 21:03:13 +0100

    adds barebone shop state

--- src/Shop.js
+++ src/Shop.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from 'react';
 import Navigation from './Navigation'
 
+import './Shop.css';
+
 const Shop = () => {
-  return (
+    const [items, setItems] = useState(["shoes", "cartoons", "dog", "cat"]);
+    return (
     <div>
         <Navigation />
+
         <h1>Hello from Shop</h1>
+        {items.map((item) =>
+            <div className="item">{item}</div>
+        )}
+        
     </div>
   );
 };
